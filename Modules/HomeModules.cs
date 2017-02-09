2f6658112219a15cc675fdc6f131ed751c6a9a74
Caitlin Hines and Cassie Musolf <caitlin+cassie@>
2017-02-09 10:33:43 -0800

    cd project is working

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,55 @@
+using Nancy;
+using cds.Objects;
+using System.Collections.Generic;
+
+namespace cds
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+      Get["/artists"] = _ => {
+        var allArtists = Artist.GetAll();
+        return View["artists.cshtml", allArtists];
+      };
+      Get["/artists/new"] = _ => {
+        return View["artist_form.cshtml"];
+      };
+      Post["/artists"] = _ => {
+        var newArtist = new Artist(Request.Form["artist-name"]);
+        var allArtists = Artist.GetAll();
+        return View["artists.cshtml", allArtists];
+      };
+      Get["/artists/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var selectedArtist = Artist.Find(parameters.id);
+        var artistAlbums = selectedArtist.GetAlbums();
+        model.Add("artist", selectedArtist);
+        model.Add("albums", artistAlbums);
+        return View["artist.cshtml", model];
+      };
+      Get["/artists/{id}/albums/new"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Artist selectedArtist = Artist.Find(parameters.id);
+        List<Album> allAlbums = selectedArtist.GetAlbums();
+        model.Add("artist", selectedArtist);
+        model.Add("albums", allAlbums);
+        return View["artist_albums_form.cshtml", model];
+      };
+      Post["/albums"] = _ => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Artist selectedArtist = Artist.Find(Request.Form["artist-id"]);
+        List<Album> artistAlbums = selectedArtist.GetAlbums();
+        string albumDescription = Request.Form["album-description"];
+        Album newAlbum = new Album(albumDescription);
+        artistAlbums.Add(newAlbum);
+        model.Add("albums", artistAlbums);
+        model.Add("artist", selectedArtist);
+        return View["artist.cshtml", model];
+      };
+    }
+  }
+}
---
+++ Objects/artists.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+
+namespace cds.Objects
+{
+  public class Album
+  {
+    private string _description;
+    private int _id;
+    private static List<Album> _instances = new List<Album>{};
+
+    public Album (string description)
+    {
+      _description = description;
+      _instances.Add(this);
+      _id = _instances.Count;
+    }
+
+    public string GetDescription()
+    {
+      return _description;
+    }
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+    public static List<Album> GetAll()
+    {
+      return _instances;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Album Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
+  }
+}
